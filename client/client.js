90849546dc4222f14691bd804a5358a78515b157
fahmed8383 <fahmed4030@gmail.com>
2023-01-14 21:17:12 -0500

    Added functionality to copy code to a device
    Co-authored-by: Umar Khan <Dragorific@users.noreply.github.com>

--- client/client.js
+++ client/client.js
@@ -1,8 +1,23 @@
 var io = require('socket.io-client');
 var socket = io.connect('http://localhost:3000', {reconnect: true, query:{"alias": "desktop 2"}});
 
-socket.emit("new message", {"question": "What is bash"});
+fetch("http://localhost:3000/api/voice-chat", {
+    method: "post",
+    body: JSON.stringify({"question": "send the code to desktop 2"}),
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+.then(function(response) {
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+}) 
+.catch(err => {
+    console.log(err);
+});
 
-socket.on("response", (data) => {
+socket.on("code", (data) => {
     console.log(data.response);
 })
